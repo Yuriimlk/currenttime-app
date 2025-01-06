34d02a7990a62223047a8038a6296db1d8ab9d0f
Yuriimlk <yurijmlk@gmail.com>
2025-01-06 13:23:25 +0100

    Added "availableTimeZones" and "coordinate" functions

--- src/main/java/com/example/currenttime_app/controller/TimeController.java
+++ src/main/java/com/example/currenttime_app/controller/TimeController.java
@@ -1,13 +1,19 @@
 package com.example.currenttime_app.controller;
 
+import com.example.currenttime_app.Config.ApiCaller;
 import com.example.currenttime_app.service.TimeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TimeController {
 
     private final TimeService timeService;
+
+    @Autowired
+    private ApiCaller apiCaller;
 
     public TimeController(TimeService timeService) {
         this.timeService = timeService;
@@ -16,6 +22,16 @@ public class TimeController {
     @GetMapping("/currentTime")
     public String currentTime() {
         return timeService.currentTime();
+    }
+
+    @GetMapping("/availableTimeZones")
+    public String[] availableTimeZones() {
+        return apiCaller.getTimeZone();
+    }
+
+    @GetMapping("/coordinate")
+    public String coordinate() {
+        return apiCaller.getCoordinate();
     }
 
 }
