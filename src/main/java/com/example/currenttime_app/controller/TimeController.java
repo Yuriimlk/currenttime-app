efdca681952e9af0f6f0a645831b4d34fb9a2100
Yuriimlk <yurijmlk@gmail.com>
2025-01-30 10:54:07 +0100

    Code refactor

--- src/main/java/com/example/currenttime_app/controller/TimeController.java
+++ src/main/java/com/example/currenttime_app/controller/TimeController.java
@@ -2,20 +2,15 @@ package com.example.currenttime_app.controller;
 
 import com.example.currenttime_app.Config.ApiCaller;
 import com.example.currenttime_app.service.TimeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TimeController {
 
     private final TimeService timeService;
 
-    @Autowired
-    private ApiCaller apiCaller;
-
-    public TimeController(TimeService timeService) {
+    public TimeController(TimeService timeService, ApiCaller apiCaller) {
         this.timeService = timeService;
     }
 
@@ -26,12 +21,12 @@ public class TimeController {
 
     @GetMapping("/availableTimeZones")
     public String[] availableTimeZones() {
-        return apiCaller.getTimeZone();
+        return timeService.availableTimeZones();
     }
 
     @GetMapping("/coordinate")
     public String coordinate() {
-        return apiCaller.getCoordinate();
+        return timeService.coordinate();
     }
 
 }
